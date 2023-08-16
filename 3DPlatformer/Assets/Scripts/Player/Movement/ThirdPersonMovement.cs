c0ae8c851f2d81474254197e8c66069b37c93a99
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2023-08-16 13:20:24 -0400

    fixed stuff
    adf

--- 3DPlatformer/Assets/Scripts/Player/Movement/ThirdPersonMovement.cs
+++ 3DPlatformer/Assets/Scripts/Player/Movement/ThirdPersonMovement.cs
@@ -16,11 +16,12 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+        
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if(direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg * cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
