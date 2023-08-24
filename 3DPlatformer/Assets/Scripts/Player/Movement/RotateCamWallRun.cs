337e2a7a5d1d25998c8b84bb4937594eab4858ce
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2023-08-24 08:42:45 -0400

    asdf
    asdf

--- 3DPlatformer/Assets/Scripts/Player/Movement/RotateCamWallRun.cs
+++ 3DPlatformer/Assets/Scripts/Player/Movement/RotateCamWallRun.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class RotateCamWallRun : MonoBehaviour
 {
+
+    public CinemachineVirtualCamera cam;
+    private CinemachinePOV cPOV;
+
+    void Start()
+    {
+        cPOV = cam.GetCinemachineComponent<CinemachinePOV>();
+    }
+
     //Rotate the camera based on if the collider is the left or right one.
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("RunnableWall"))
+        if(other.gameObject.CompareTag("RunableWall"))
         {
             if(gameObject.name == "Left")
             {
-                
+                Debug.Log("ROTATE");
+                cPOV.m_HorizontalAxis.Value = -10f; 
             }
         }
     }
