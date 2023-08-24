080dfbabb1e36d9ef393afec3758dd6da2d39d0f
Daniel Cook <addisoncook08@gmail.com>
2023-08-23 21:03:19 -0400

    Required Stuff Only?

--- 3DPlatformer/Assets/Scripts/Teleporter/Teleporter.cs
+++ 3DPlatformer/Assets/Scripts/Teleporter/Teleporter.cs
@@ -1,12 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
+    public GameObject FloatingTextPrefab;
+    private GameObject go;
+    private bool hasEntered = false;
     void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("Player")) {
-            Debug.Log("Teleporter Activated!");
+        if(other.gameObject.CompareTag("Player") && !hasEntered) {
+            hasEntered = true;
+            if (FloatingTextPrefab)
+            {
+                FloatingText(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            hasEntered = false;
+            if (FloatingTextPrefab)
+            {
+                FloatingText(false);
+            }
+        }
+    }
+
+    void FloatingText(bool create)
+    {
+        if (create)
+        {
+            go = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+            go.GetComponent<TextMesh>().text = "E";
+            
+        } else
+        {
+            GameObject.Destroy(go);
         }
     }
 }
