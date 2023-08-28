8e2f6020da50add9959aed287147ce427e02f4be
Daniel Cook <addisoncook08@gmail.com>
2023-08-28 12:36:26 -0400

    Teleporter

--- 3DPlatformer/Assets/Scripts/Teleporter/Teleporter.cs
+++ 3DPlatformer/Assets/Scripts/Teleporter/Teleporter.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teleporter : MonoBehaviour
 {
     public GameObject FloatingTextPrefab;
+    public string SceneToLoad;
+
     private GameObject go;
     private bool hasEntered = false;
+
+    void Update() {
+        if (hasEntered && Input.GetKeyDown(KeyCode.E)) {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Player") && !hasEntered) {
             hasEntered = true;
