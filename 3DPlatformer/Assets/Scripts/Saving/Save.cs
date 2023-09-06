fde89b2dbbf030c3536ee71f88bfcd140a6848f2
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2023-09-06 13:19:16 -0400

    sdafasdf
    asdfasdfa

---
+++ 3DPlatformer/Assets/Scripts/Saving/Save.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour
+{
+    public void saveTutorial()
+    {
+
+    }
+    public void saveLevel1()
+    {
+
+    }
+    public void saveLevel2()
+    {
+
+    }
+    public void saveLevel3()
+    {
+
+    }
+    public void saveLevel4()
+    {
+
+    }
+    public void saveLevel5()
+    {
+        
+    }
+}
